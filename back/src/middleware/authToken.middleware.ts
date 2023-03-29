583bd4e9e0c15ba7fb50dae8ee21b0de701079c2
Juliana Tissiani Diorio <julianatdiorio@gmail.com>
2023-03-28 22:17:33 -0300

    fix: edit authToken

--- back/src/middleware/authToken.middleware.ts
+++ back/src/middleware/authToken.middleware.ts
@@ -1,33 +1,3 @@
-// import { AppError } from "./../errors/AppError";
-// import { Request, Response, NextFunction } from "express";
-// import "dotenv/config";
-// import jwt from "jsonwebtoken";
-
-// export const verifyTokenMiddleware = async (
-//   req: Request,
-//   res: Response,
-//   next: NextFunction
-// ) => {
-//   let token = req.headers.authorization;
-//   if (!token) {
-//     throw new AppError("Invalid token", 401);
-//   }
-//   token = token.split(" ")[1];
-//   jwt.verify(token, process.env.SECRET_KEY, (error, decoded: any) => {
-//     if (error) {
-//       throw new AppError(`${error}`, 401);
-//     }
-
-//     req.user = {
-//       id: decoded.sub,
-//       isActive: decoded.isActive,
-//       isAdm: decoded.isAdm,
-//     };
-
-//     return next();
-//   });
-// };
-
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 require("dotenv").config();
